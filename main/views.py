9aad2f0f26a6cc3e175b856627f28ec115c835b2
akakshay2607 <akshay.05775@gmail.com>
2025-07-23 11:56:45 +0530

    added telegram

--- main/views.py
+++ main/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render,HttpResponseRedirect
 from .forms import ContactForm
 from django.urls import reverse
 from django.http import JsonResponse
+import requests
+import json
 
-# Create your views here.
+def send_telegram_message(message):
+    bot_token = '6391769840:AAHGMZ5fl7xQUpFVsANpIsTXD9fPCQ9dw6U'
+    chat_id = '291876635'
+    url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
+    payload = {
+        'chat_id': chat_id,
+        'text': message
+    }
+    response = requests.post(url, data=payload)
+    return response.json()
 
 def home(request):
     return render(request, 'main/index.html')
@@ -19,6 +30,7 @@ def contact(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             form.save()
+            resp = send_telegram_message(json.dumps(form.cleaned_data))
             return JsonResponse({'message': 'Thanks! We will get back to you shortly.'})
         else:
             return JsonResponse({'error': form.errors.as_text()}, status=400)
